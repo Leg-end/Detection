d66a971ef7fb3a34c295e207f2e90fee5f2159d5
Leg-end <31924601+Leg-end@users.noreply.github.com>
2019-06-02 13:33:08 +0800

    Create unit_test.py

---
+++ utils/unit_test.py
@@ -0,0 +1,31 @@
+import os
+import tensorflow as tf
+import time
+from utils import proposal_util
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+
+def test_anchors():
+    anchors = proposal_util.generate_anchors()
+    with tf.Session() as sess:
+        print(sess.run(anchors))
+
+
+def test_image_anchors():
+    anchors, num = proposal_util.generate_image_anchors(224, 224)
+    with tf.Session() as sess:
+        print(sess.run([anchors, num]))
+
+
+def test_target_anchors():
+    overlap = tf.constant([[[.1], [.2], [.3], [.5], [.7], [.2]],
+                           [[.9], [.4], [.6], [.1], [.2], [.1]],
+                           [[.3], [.6], [.6], [.1], [.6], [.2]]])
+    p = proposal_util.generate_positives_negatives(overlap)
+    print(p)
+
+
+if __name__ == "__main__":
+    t = time.time()
+    test_target_anchors()
+    print(time.time()-t)
